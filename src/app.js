6b4fa76375cdc831778edba679f9f6526f08c6f6
Gary Ascuy <gary.ascuy@jalasoft.com>
2017-07-09 14:00:11 -0400

    Improving format

--- src/app.js
+++ src/app.js
@@ -3,19 +3,19 @@ import { log } from './logger'
 const game = new Phaser.Game(800, 680, Phaser.AUTO, 'Web Celestial War', {preload, create})
 
 function preload() {
-	log('Staring Web Celestial War')
+  log('Staring Web Celestial War')
 
-	game.load.crossOrigin='Anonymous'
-	game.load.bitmapFont('font',
-		'https://dl.dropboxusercontent.com/s/z4riz6hymsiimam/font.png?dl=0',
-		'https://dl.dropboxusercontent.com/s/7caqsovjw5xelp0/font.xml?dl=0'
-	)
-	game.load.image('hex', 'https://dl.dropboxusercontent.com/s/rhwagtrs8o2v0cz/hexsmall.png?dl=0')
+  game.load.crossOrigin='Anonymous'
+  game.load.bitmapFont('font',
+    'https://dl.dropboxusercontent.com/s/z4riz6hymsiimam/font.png?dl=0',
+    'https://dl.dropboxusercontent.com/s/7caqsovjw5xelp0/font.xml?dl=0'
+  )
+  game.load.image('hex', 'https://dl.dropboxusercontent.com/s/rhwagtrs8o2v0cz/hexsmall.png?dl=0')
 }
 
 function create() {
   game.stage.backgroundColor = '#fff'
   const infoTxt = game.add.text(10, 30, 'Hello World')
-	game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
-	game.input.holdRate = 500
+  game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL
+  game.input.holdRate = 500
 }
